02f7452c0f0a823395502461c0c12980b314050b
Jdasi <Joseph2.Dasilva@live.uwe.ac.uk>
2016-05-26 12:58:23 +0100

    Added Player Movement

---
+++ Unity Folder/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private int currentLane = 0;
+    private Transform player;
+    public int playerID;
+    public GameObject[] deskArray;
+
+	void Start() 
+	{
+        player = GameObject.Find("Player" + playerID).transform;
+        deskArray = GameObject.FindGameObjectsWithTag("DESK");
+	}
+	
+	void Update() 
+	{
+        ProcessMovement(playerID);
+	}
+
+    public void ProcessMovement(int id)
+    {
+        string axis = "Vertical" + id;
+
+        if (Input.GetButtonDown(axis) && Input.GetAxisRaw(axis) > 0f)
+        {
+            if (currentLane == deskArray.Length - 1)
+                return;
+            
+            TranslatePlayerY(2f);
+            ++currentLane;
+        }
+        
+        if (Input.GetButtonDown(axis) && Input.GetAxisRaw(axis) < 0f)
+        {
+            if (currentLane == 0)
+                return;
+            
+            TranslatePlayerY(-2f);
+            --currentLane;
+        }
+    }
+
+    public void TranslatePlayerY(float amount)
+    {
+        player.position = new Vector3(player.position.x, player.position.y + amount, 0);
+    }
+}
