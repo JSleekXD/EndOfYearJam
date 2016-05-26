21b1cb0010866da00aec1ec089a4829a326a6b64
Jdasi <silvacore@hotmail.co.uk>
2016-05-26 16:16:01 +0100

    Added end scene with menu

---
+++ Unity Folder/Assets/Scripts/EndMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndMenuManager : MonoBehaviour 
+{
+	public void RestartButton()
+    {
+        Application.LoadLevel("Joe's Scene");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
