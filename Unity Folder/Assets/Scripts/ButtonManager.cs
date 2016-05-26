a2880f2e989a8334dfc025114f1d195051573db4
Phalanx <silvacore@hotmail.co.uk>
2016-05-26 14:11:16 +0100

    Added main menu

---
+++ Unity Folder/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour 
+{
+	public void PlayButton()
+    {
+        Application.LoadLevel("Joe's Scene");
+    }
+}
