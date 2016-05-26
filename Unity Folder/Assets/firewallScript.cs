3575607a2d122769f6936d7e234c3311f3809fc4
DudleyHK <dudleydawes@hotmail.co.uk>
2016-05-26 15:04:24 +0100

    Dudleys Scene
    Pretty basic. Drag the red thing into the blue firewall to make it
    disappear.

---
+++ Unity Folder/Assets/firewallScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firewallScript : MonoBehaviour 
+{
+    private GameObject projectile;
+    
+    
+    // Use this for initialization
+    void Awake () 
+    {
+        projectile = GameObject.FindGameObjectWithTag("Projectile");     
+        gameObject.SetActive(true);
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject == projectile)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
