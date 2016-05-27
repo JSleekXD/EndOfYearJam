2afcd1f6c2dbade07a56e27f47de648707ec1338
Julian Stopher <Julian2.Stopher@live.uwe.ac.uk>
2016-05-27 10:58:25 +0100

    created PlayerProperties
    This handles the playerID and health, which all the other scripts
    inherit.

---
+++ Unity Folder/Assets/PlayerProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProperties : MonoBehaviour {
+
+	public int playerID;
+	public float playerHealth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
