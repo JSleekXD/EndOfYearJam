7b7ea6e0ae3b31d20ba4dd287e760c0f1e7ece08
DudleyHK <dudleydawes@hotmail.co.uk>
2016-05-27 21:13:13 +0100

    need to change it depending on the row the player is on

--- Unity Folder/Assets/FirewallController.cs
+++ Unity Folder/Assets/FirewallController.cs
@@ -7,7 +7,6 @@ public class FirewallController : MonoBehaviour
 	public List<GameObject> p1List = new List<GameObject>();
 	public List<GameObject> p2List = new List<GameObject>();
 
-	public GameObject firewall;
 
 
 	// Use this for initialization
@@ -22,64 +21,19 @@ public class FirewallController : MonoBehaviour
 	
 	}
 
-	public bool SelectList(int playerID)
+	public List<GameObject> SelectList(int playerID)
 	{
-		bool isPlaced = false;
+		List<GameObject> temp = new List<GameObject> ();
 
-		if (playerID == 0) 
-		{
-			isPlaced = ListCountCheck (p1List, playerID);
-		}
-
-		if (playerID == 1) 
-		{
-			isPlaced = ListCountCheck (p2List, playerID);
-		}
-
-		// CHECK IF THE FIREWALL CAN BE PLACED
-		if (isPlaced) 
-			return true;
-		else
-			return false;
-	}
-
-	bool ListCountCheck(List<GameObject> list, int playerID)
-	{
-		if (list.Count == 0) 
-		{
-			// ADD ONE TO THE DESK AT x POSITION
-			PlaceFirewall(list, 0, playerID, 2);
-		}
-		else if(list.Count == 1)
-		{
-			// ADD ONE TO THE DESK AT x + 1 POSITION
-		}
-		else if(list.Count == 2)
-		{
-			// ADD ONE TO THE DESK AT x + 2 POSITION
-		}
-		else
-		{
-			// FIREWALL CANNOT BE PLACED
-			return false;
-		}
-
-		return true;
-	}
-
-	void PlaceFirewall(List<GameObject> list, int listPos, int playerID, int positionValue)
-	{
 		switch (playerID) 
 		{
 		case 0:
-			GameObject p1Firewall = (GameObject)Instantiate (firewall, new Vector3 (transform.position.x + positionValue, transform.position.y, transform.position.z), transform.rotation);
-			list [listPos] = p1Firewall; // PUSH OBJECT INTO LIST
+			temp = p1List;
 			break;
-		//	firewellClone.GetComponent<Projectile> ().projectileID = playerID;
 		case 1:
-			GameObject p2Firewall = (GameObject)Instantiate (firewall, new Vector3 (transform.position.x - positionValue, transform.position.y, transform.position.z), transform.rotation);
-			list [listPos] = p2Firewall; // PUSH OBJECT INTO LIST
+			temp = p2List;
 			break;
 		}
+		return temp;
 	}
 }
--- Unity Folder/Assets/Scripts/BuildFirewall.cs
+++ Unity Folder/Assets/Scripts/BuildFirewall.cs
@@ -8,6 +8,9 @@ public class BuildFirewall : MonoBehaviour
 	public int playerID; 
 	public int currentLane;
 	public GameObject firewallManager;
+	public List<GameObject> firewallList = new List<GameObject> ();
+	public GameObject firewallObject;
+
 	private bool isCreated;
 	private FirewallController firewallControllerScript;
 	private PlayerProperties playerProperties;
@@ -30,14 +33,53 @@ public class BuildFirewall : MonoBehaviour
 	{
 		if(Input.GetKeyDown(KeyCode.Space) && playerID == 0)
 		{
-			Debug.Log("SPACE PRESSED" + playerID);
-			firewallControllerScript.SelectList(playerID);
+			Debug.Log("SPACE PRESSED" + firewallList.Count);
+			firewallList = firewallControllerScript.SelectList(playerID);
 		}
 		
 		if(Input.GetKeyDown(KeyCode.Return) && playerID == 1)
 		{
-			Debug.Log("RETURN PRESSED" + playerID);
-			firewallControllerScript.SelectList(playerID);
+			Debug.Log("RETURN PRESSED" + firewallList.Count);
+			firewallList = firewallControllerScript.SelectList(playerID);
+		}
+	}
+
+	bool ListCountCheck(int playerID)
+	{
+		if (firewallList.Count == 0) 
+		{
+			// ADD ONE TO THE DESK AT x POSITION
+			PlaceFirewall(playerID, 2);
+		}
+		else if(firewallList.Count == 1)
+		{
+			PlaceFirewall(playerID, 3);
+		}
+		else if(firewallList.Count == 2)
+		{
+			PlaceFirewall(playerID, 4);
+		}
+		else
+		{
+			// FIREWALL CANNOT BE PLACED
+			return false;
+		}
+		
+		return true;
+	}
+	
+	void PlaceFirewall(int playerID, int positionValue)
+	{
+		switch (playerID) 
+		{
+		case 0:
+			GameObject p1Firewall = (GameObject)Instantiate (firewallObject, new Vector3 (transform.position.x + positionValue, transform.position.y, transform.position.z), transform.rotation);
+			firewallList.Add(p1Firewall); // PUSH OBJECT INTO LIST
+			break;
+		case 1:
+			GameObject p2Firewall = (GameObject)Instantiate (firewallObject, new Vector3 (transform.position.x - positionValue, transform.position.y, transform.position.z), transform.rotation);
+			firewallList.Add(p2Firewall); // PUSH OBJECT INTO LIST
+			break;
 		}
 	}
 }
