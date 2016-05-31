01e9f1596fd2b89bdd7fa788df8bca4e99d907e9
Julian Stopher <Julian2.Stopher@live.uwe.ac.uk>
2016-05-31 15:35:46 +0100

    firewall building sound now works properly, player has a personal audio manager

--- Unity Folder/Assets/Scripts/Scene/AudioManager.cs
+++ Unity Folder/Assets/Scripts/Scene/AudioManager.cs
@@ -5,15 +5,18 @@ public class AudioManager : MonoBehaviour {
 
 	public GameObject roundOverSound;
 
+
 	public static AudioManager instance;
 
 	public void PlayRoundOverSound(){
-	//	roundOverSound.enabled = true;
 		roundOverSound.SetActive (true);
 	}
 	public void StopRoundOverSound(){
 		roundOverSound.SetActive (false);
 	}
+
+
+
 
 
 }
---
+++ Unity Folder/Assets/Scripts/Scene/PlayerAudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAudioManager : MonoBehaviour {
+
+	public GameObject firewallBuildingSound;
+
+	public void PlayFirewallBuildingSound(){
+
+		firewallBuildingSound.GetComponent<AudioSource> ().volume = 1;
+	}
+	public void StopFirewallBuildingSound(){
+		firewallBuildingSound.GetComponent<AudioSource> ().volume = 0;
+	}
+}
