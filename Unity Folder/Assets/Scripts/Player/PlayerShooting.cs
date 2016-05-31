956fd6aadb80d6fee2b16049ac43fca4de0a3579
DudleyHK <dudleydawes@hotmail.co.uk>
2016-05-31 22:03:47 +0100

    change orientation of viruses : FEATURE COMPLETE
    This can be changed and editted in the Player Shooting script. SCALE is
    edited in the Inspector, under Player Shooting

--- Unity Folder/Assets/Scripts/Player/PlayerShooting.cs
+++ Unity Folder/Assets/Scripts/Player/PlayerShooting.cs
@@ -8,7 +8,9 @@ public class PlayerShooting : MonoBehaviour
 	public List<GameObject> projectiles;
     public float projectileSpeed = 0.25f;
     public float offsetFromPlayer = 1f;
-		
+
+	private Quaternion playerOneSpawnRot = new Quaternion(180.0f, 180.0f, 0.0f, 0.0f);
+	private Quaternion playerTwoSpawnRot = new Quaternion(217.0f, -217.0f, 0.0f, 0.0f);
 	public float newScaleX = 2.0f;
 	public float newScaleY = 2.0f;
     
@@ -25,11 +27,19 @@ public class PlayerShooting : MonoBehaviour
 	{
         if (projectiles.Count >= 3)
             return;
+
+		// Depending on the playerID change the rotation of the projectile
 		gameObject.GetComponent<PlayerControl> ().audioManager.PlayShootVirusSound ();
         GameObject projectileInstance = (GameObject)Instantiate(projectile, new Vector3(player.transform.position.x + offsetFromPlayer, player.transform.position.y, player.transform.position.z), player.transform.rotation);
 		projectileInstance.transform.localScale = new Vector2 (newScaleX, newScaleY);
 		projectileInstance.name = "Player" + playerID + "Projectile";
         projectileInstance.GetComponent<Projectile>().SetProperties(playerID, projectileSpeed, player.GetComponent<PlayerMovement>().CurrentLane);
+
+		if (playerID == 0)
+			projectileInstance.transform.rotation = playerOneSpawnRot;
+		if(playerID == 1)
+			projectileInstance.transform.rotation = playerTwoSpawnRot;
+
 		projectiles.Add(projectileInstance);
 	}
 
